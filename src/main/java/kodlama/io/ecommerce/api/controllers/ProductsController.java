34565a6c6b851f3bdffba07cdd30a42eff6ff7cc
eamre <emrealkan.1@outlook.com>
2023-03-22 16:23:24 +0300

    project upload

---
+++ src/main/java/kodlama/io/ecommerce/api/controllers/ProductsController.java
@@ -0,0 +1,44 @@
+package kodlama.io.ecommerce.api.controllers;
+
+import kodlama.io.ecommerce.business.abstracts.ProductService;
+import kodlama.io.ecommerce.entities.concretes.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/products")
+public class ProductsController {
+    ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+    @GetMapping
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable int id){
+        return productService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void add(@RequestBody Product product){
+        productService.add(product);
+    }
+
+    @PutMapping ("/{id}")
+    public void update(@PathVariable int id,Product product){
+        productService.update(id,product);
+    }
+
+    @DeleteMapping ("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id){
+        productService.delete(id);
+    }
+}
---
+++ src/main/java/kodlama/io/ecommerce/business/concretes/ProductManager.java
@@ -0,0 +1,75 @@
+package kodlama.io.ecommerce.business.concretes;
+
+import kodlama.io.ecommerce.business.abstracts.ProductService;
+import kodlama.io.ecommerce.entities.concretes.Product;
+import kodlama.io.ecommerce.repository.abstracts.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProductManager implements ProductService {
+    ProductRepository productRepository;
+
+    public ProductManager(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    @Override
+    public Product getById(int id) {
+        return productRepository.getById(id);
+    }
+
+    @Override
+    public Product add(Product product) {
+        validateProduct(product);
+        return productRepository.add(product);
+    }
+
+    @Override
+    public void delete(int id) {
+        productRepository.delete(id);
+    }
+
+    @Override
+    public Product update(int id,Product product) {
+        validateProduct(product);
+        return productRepository.update(id,product);
+
+    }
+
+
+    //! Business rules
+    private void validateProduct(Product product) {
+        checkIfUnitPriceValid(product);
+        checkIfQuantityValid(product);
+        checkIfDescriptionLengthValid(product);
+    }
+
+    private void checkIfUnitPriceValid(Product product) {
+        if (product.getPrice() <= 0)
+            throw new IllegalArgumentException("Price cannot be less than or equal to zero.");
+    }
+
+    private void checkIfQuantityValid(Product product) {
+        if (product.getQuantity() < 0) throw new IllegalArgumentException("Quantity cannot be less than zero.");
+    }
+
+    private void checkIfDescriptionLengthValid(Product product) {
+        if (product.getDescription().length() < 10 || product.getDescription().length() > 50)
+            throw new IllegalArgumentException("Description length must be between 10 and 50 characters.");
+    }
+}
+//if (product.getPrice()<=0){
+//        throw new RuntimeException("ürünün fiyatı sıfırdan küçük olamaz ");
+//        }
+//        if(product.getQuantity()<0){
+//        throw new RuntimeException("ürünün miktarı sıfırdan küçük olamaz ");
+//        }
+//        if(!(product.getDescription().length() >= 10 && product.getDescription().length() <= 50)){
+//        throw new RuntimeException("ürünün açıklama kısmı 10 ile 50 karakter arasında olmalı ");
+//        }
